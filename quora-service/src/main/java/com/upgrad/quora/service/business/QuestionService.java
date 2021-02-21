09b46f4404f386ba1b21bc2d24f2c4f66401895b
ShubhamDhoot <shubham.dhoot30@gmail.com>
2021-02-21 17:44:13 +0530

    Added apis for getAllQuestionsByUser and deleteQuestion

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -2,9 +2,11 @@ package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -16,6 +18,8 @@ import java.util.List;
 public class QuestionService {
     @Autowired
     private QuestionDao questionDao;
+    @Autowired
+    private CommonService commonService;
 
     /**
      *
@@ -66,9 +70,49 @@ public class QuestionService {
         return updatedQuestion;
     }
 
+
     public void authorizeEdit(UserEntity userToEdit,UserEntity questionOwner) throws AuthorizationFailedException {
         if(userToEdit.getId() != questionOwner.getId()){
             throw new AuthorizationFailedException("ATHR-003","Only the question owner can edit the question");
         }
     }
+
+
+  /**
+     *
+     * @param userUuid- uuid of user for which all question need to fetch
+     * @return list of question for user
+     *
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public List<QuestionEntity> getAllQuestionsByUser (final String userUuid) throws UserNotFoundException {
+
+        UserEntity userEntity=commonService.getUserByUuid(userUuid);
+        List<QuestionEntity> questionEntityList=questionDao.getAllQuestionsByUser(userEntity.getId());
+        return questionEntityList;
+    }
+
+    /**
+     *
+     * @param userAuthTokenEntity - authorized user entity
+     * @param questionUuid - uuid of question which need to be deleted
+     * @return question entity which was deleted
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity deleteQuestion(final UserAuthEntity userAuthTokenEntity, final String questionUuid) throws AuthorizationFailedException, InvalidQuestionException {
+
+
+        QuestionEntity questionEntity=questionDao.getQuestion(questionUuid);
+        if(questionEntity==null){
+            throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
+        }
+
+        if(userAuthTokenEntity.getUserEntity().getId()!=questionEntity.getUserEntity().getId() && questionEntity.getUserEntity().getRole()!="admin"){
+            throw new AuthorizationFailedException("ATHR-003","Only the question owner or admin can delete the question");
+        }
+        questionDao.deleteQuestion(questionEntity);
+        return  questionEntity;
+    }
+
+
 }
